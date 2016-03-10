89a85d5abf32d1a7703bb9fda91793d0eedd843f
paxilpaz <paxilpaz@gmail.com>
2016-03-10 14:15:25 +0000

    Added Settings activity

--- app/src/main/java/com/example/paxilpaz/movieapp/MainActivity.java
+++ app/src/main/java/com/example/paxilpaz/movieapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.paxilpaz.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
             return true;
         }
 
