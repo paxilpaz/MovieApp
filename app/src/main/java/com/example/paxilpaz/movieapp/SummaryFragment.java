f4b5dfc42a39b991d553a74bb149dd58f322802a
paxilpaz <paxilpaz@gmail.com>
2016-03-10 16:13:39 +0000

    Updated a bit of SummaryFragment with a void MovieArrayAdapter... app does not crash!

--- app/src/main/java/com/example/paxilpaz/movieapp/SummaryFragment.java
+++ app/src/main/java/com/example/paxilpaz/movieapp/SummaryFragment.java
@@ -1,10 +1,14 @@
 package com.example.paxilpaz.movieapp;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.paxilpaz.movieapp.movie.Movie;
+import com.example.paxilpaz.movieapp.movie.MovieArrayAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,14 +17,31 @@ public class SummaryFragment extends Fragment {
 
     private static final String LOG_CAT = SummaryFragment.class.getSimpleName();
 
+    private MovieArrayAdapter movieArrayAdapter;
+
+
     public SummaryFragment() {
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    private void updateMovies() {
+        movieArrayAdapter = new MovieArrayAdapter(getActivity().getApplicationContext(),
+                R.layout.my_image_view,
+                new Movie[] { });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridView = (GridView)rootView.findViewById(R.id.gridView);
+        gridView.setAdapter(movieArrayAdapter);
         return rootView;
     }
 }
