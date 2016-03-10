25ff9482611d9834f472db42f0d25311ce6359b0
paxilpaz <paxilpaz@gmail.com>
2016-03-10 13:43:21 +0000

    Created layout for Settings menu

--- app/src/main/java/com/example/paxilpaz/movieapp/MainActivity.java
+++ app/src/main/java/com/example/paxilpaz/movieapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
         setSupportActionBar(toolbar);
     }
 
